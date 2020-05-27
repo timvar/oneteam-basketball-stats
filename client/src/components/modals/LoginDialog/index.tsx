1a13b8d5b5ebda202f0825021305423218c817bf
Timo Varila <timo.varila@siili.com>
2020-05-27 12:20:24 +0300

    login modal

---
+++ client/src/components/modals/LoginDialog/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import { User } from '../../../store/user/types';
+
+interface Props {
+  modalOpen: boolean;
+  onClose: () => void;
+  onSubmit: (user: User) => void;
+}
+
+const LoginDialog: React.FC<Props> = ({ modalOpen, onClose, onSubmit }) => {
+  const [username, setUsername] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const user = {
+      username,
+      password,
+    };
+    setUsername('');
+    setPassword('');
+    onSubmit(user);
+  };
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+    console.log(username);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+    console.log(password);
+  };
+
+  return (
+    <div>
+      <Dialog open={modalOpen} onClose={onClose}>
+        <DialogContent>
+          <form onSubmit={handleSubmit} noValidate autoComplete="off">
+            <TextField
+              autoFocus={true}
+              fullWidth
+              style={{ marginBottom: 16 }}
+              name="username"
+              onChange={handleUsernameChange}
+              placeholder="Username"
+              required={true}
+              size="small"
+              type="text"
+              value={username}
+              variant="outlined"
+            />
+            <TextField
+              fullWidth
+              style={{ marginBottom: 24 }}
+              name="password"
+              onChange={handlePasswordChange}
+              placeholder="Password"
+              required={true}
+              size="small"
+              type="password"
+              value={password}
+              variant="outlined"
+            />
+            <Button
+              variant="outlined"
+              type="submit"
+              color="primary"
+              style={{ marginBottom: 8, marginRight: 8 }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={onClose}
+              color="secondary"
+              style={{ marginBottom: 8, marginLeft: 8 }}
+            >
+              Cancel
+            </Button>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default LoginDialog;
--- client/src/views/Login/index.tsx
+++ client/src/views/Login/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import LoginDialog from '../../components/modals/LoginDialog';
+import { User } from '../../store/user/types';
 
 const Login: React.FC = () => {
-  return <></>;
+  const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
+
+  const openDialog = (): void => {
+    setDialogOpen(true);
+  };
+  const closeDialog = (): void => {
+    setDialogOpen(false);
+  };
+  const handleLogin = (values: User): void => {
+    closeDialog();
+    // dispatch(addPlayer(values));
+    console.log('user: ', values);
+  };
+  return (
+    <>
+      <Button variant="outlined" onClick={() => openDialog()}>
+        Login
+      </Button>
+
+      <LoginDialog
+        modalOpen={dialogOpen}
+        onClose={closeDialog}
+        onSubmit={handleLogin}
+      />
+    </>
+  );
 };
 
 export default Login;
